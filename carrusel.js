1f2389fdbe7160dd02a4b84489910e8c4e54d9d2
victorakio125 <higuchiv@gmail.com>
2023-05-09 11:32:40 -0500

    nose

---
+++ carrusel.js
@@ -0,0 +1,34 @@
+function change_text(){
+    document.getElementById("demo").innerHTML = "Obtuviste un 10% de descuento recuerda tomarme una foto como prueba!";
+    document.getElementById("dummy").innerHTML = "EASTER EGG ENCONTRADO";
+    document.getElementById("dummy").style.color = "white"
+}
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
