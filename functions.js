acaee8e5bdb4c489db42732997074300b4e74a58
victorakio125 <higuchiv@gmail.com>
2023-05-30 10:33:25 -0500

    yep

---
+++ functions.js
@@ -0,0 +1,5 @@
+function change_text(){
+    document.getElementById("demo").innerHTML = "Obtuviste un 10% de descuento recuerda tomarme una foto como prueba!";
+    document.getElementById("dummy").innerHTML = "EASTER EGG ENCONTRADO";
+    document.getElementById("dummy").style.color = "white"
+}
